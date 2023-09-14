845b253ee2923f2e969f6b94621806751b24e913
RamosAngie <angelica.ramos.wk@gmail.com>
2023-09-14 15:53:05 -0300

    realizados ejercicios 2 y 3

--- Explicación/variables.js
+++ Explicación/variables.js
@@ -19,4 +19,27 @@
     Hay variable que se llaman constantes eso es porque desde que la creamos no se puede cambiar el valor de lo que guarda
     
     const numeros = 8;
+
+    Operadores logicos:
+
+    < (menor a) ej: 8 < 15 = Verdadero
+    > (mayor a) ej: 8 > 2 = Verdadero
+    <= (menor o igual) 8 <= 8 = Verdadero
+    >= (mayor o igual) 8 >= 15 = Falso
+
+    == (Igual a) 2 == "2" -> Verdadero. (Aca solo el valor tiene que ser igual)
+    === (Identico a) 2 === "2" -> Falso. (Aca tanto el valor como el tipo deben ser iguales)
+    != (Distinto de) 1 != 2 -> Verdadero.
 */
+
+//Generar una funcion que le pase 2 datos por parametro y me devuelva la suma.
+
+function sumar (n1,n2){
+    var resultado;
+    resultado = n1+n2;
+    return resultado;
+}
+
+var resultadoFuncion;
+resultadoFuncion = sumar(8,10);
+consola.log(resultadoFuncion);
--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -4,6 +4,7 @@
 function devolverString(string) {
    // Debe retornar un string.
    // Tu código:
+   return string;
 }
 
 // ⛔️ "X" e "Y" son números.
@@ -11,26 +12,39 @@ function devolverString(string) {
 function suma(x, y) {
    // Retorna el resultado de su suma.
    // Tu código:
+   var rsuma = x+y;
+   return rsuma;
 }
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código:
+   var rresta = x - y;
+   return rresta;
 }
 
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
+   var rdividir = x / y;
+   return rdividir;
+
 }
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
    // Tu código:
+   var rmultiplicar = x * y;
+   return rmultiplicar;
+
 }
 
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
    // Tu código:
+   var rresto = x % y;
+   return rresto;
+   
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
